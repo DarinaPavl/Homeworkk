6c2b42f00ac19b9d17fd0c4dcaaadb3728f1bea8
DarinaPavl <sovukh_d@vk.com>
2025-04-12 14:12:01 +0500

    cheange home2

--- Hhh.sql
+++ Hhh.sql
@@ -1,47 +1,46 @@
-create table if not exists Genres(
-  id serial primary key,
-  name varchar(50) unique not null
+CREATE TABLE IF NOT EXISTS Genres (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) UNIQUE NOT NULL
 );
 
-create table if not exists Performers(
-  id serial primary key,
-  name varchar(50) not null
+CREATE TABLE IF NOT EXISTS Performers (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
 );
 
-create table if not exists Genres_Performers(
-  genre_id integer references Genres(id),
-  performer_id integer references Performers(id),
-  constraint pk1 primary key (genre_id, performer_id)
+CREATE TABLE IF NOT EXISTS Genres_Performers (
+  genre_id INTEGER REFERENCES Genres(id),
+  performer_id INTEGER REFERENCES Performers(id),
+  CONSTRAINT pk1 PRIMARY KEY (genre_id, performer_id)
 );
 
-create table if not exists Albums(
-  id serial primary key,
-  name varchar(50) not null,
-  year integer not null check(year > 0)
+CREATE TABLE IF NOT EXISTS Albums (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  year INTEGER NOT NULL CHECK(year > 0)
 );
 
-create table if not exists Performers_Albums(
-  performer_id integer references Performers(id),
-  album_id integer references Albums(id),
-  constraint pk2 primary key (performer_id, album_id)
+CREATE TABLE IF NOT EXISTS Performers_Albums (
+  performer_id INTEGER REFERENCES Performers(id),
+  album_id INTEGER REFERENCES Albums(id),
+  CONSTRAINT pk2 PRIMARY KEY (performer_id, album_id)
 );
 
-create table if not exists Tracks(
-  id serial primary key,
-  album_id integer references Albums(id),
-  name varchar(50) not null,
-  duration numeric not null,
-  check (duration > 0)
+CREATE TABLE IF NOT EXISTS Tracks (
+  id SERIAL PRIMARY KEY,
+  album_id INTEGER REFERENCES Albums(id),
+  name VARCHAR(50) NOT NULL,
+  duration NUMERIC NOT NULL CHECK(duration > 0)
 );
 
-create table if not exists Collections(
-  id serial primary key,
-  name varchar(50) not null,
-  year integer not null check(year > 0)
+CREATE TABLE IF NOT EXISTS Collections (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  year INTEGER NOT NULL CHECK(year > 0)
 );
 
-create table if not exists Collections_Tracks(
-  collection_id integer references Collections(id),
-  track_id integer references Tracks(id),
-  constraint pk3 primary key (collection_id, track_id)
+CREATE TABLE IF NOT EXISTS Collections_Tracks (
+  collection_id INTEGER REFERENCES Collections(id),
+  track_id INTEGER REFERENCES Tracks(id),
+  CONSTRAINT pk3 PRIMARY KEY (collection_id, track_id)
 );
